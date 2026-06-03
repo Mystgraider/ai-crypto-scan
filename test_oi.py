83c502f5b583c0dd00cbe9b66dd39a0f0602e4d5
mystgraider <122290557+Mystgraider@users.noreply.github.com>
2026-06-03 20:28:30 +0800

    Add test for OIEngine update method

---
+++ test_oi.py
@@ -0,0 +1,17 @@
+from oi_engine import OIEngine
+
+oi = OIEngine()
+
+print(
+    oi.update(
+        "BTC",
+        1000000
+    )
+)
+
+print(
+    oi.update(
+        "BTC",
+        1100000
+    )
+)
