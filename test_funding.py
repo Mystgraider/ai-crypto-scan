8fbbec7338dc3098148341054fc7079a92a5f294
mystgraider <122290557+Mystgraider@users.noreply.github.com>
2026-06-03 20:33:07 +0800

    Add tests for FundingEngine analyze method

---
+++ test_funding.py
@@ -0,0 +1,15 @@
+from funding_engine import FundingEngine
+
+engine = FundingEngine()
+
+print(
+    engine.analyze(
+        0.0001
+    )
+)
+
+print(
+    engine.analyze(
+        0.0007
+    )
+)
