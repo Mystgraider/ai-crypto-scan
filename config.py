5971e7119aa57c68afc8d095c200eff798688a0b
mystgraider <122290557+Mystgraider@users.noreply.github.com>
2026-06-03 20:54:37 +0800

    Add configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+CONFIG = {
+
+    # Scanner
+
+    "top_coins_limit": 300,
+
+    "min_score": 70,
+
+    "min_rr": 2.0,
+
+    # Risk
+
+    "min_sl_pct": 0.005,
+
+    # Cooldown
+
+    "signal_cooldown_hours": 12,
+
+    # Versions
+
+    "db_version": "1.0.0",
+
+    "config_version": "1.0.0",
+
+    "strategy_version": "5.0.0"
+}
